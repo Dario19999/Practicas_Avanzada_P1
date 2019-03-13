39e891fc02c94fe373e9a2c34f0efac073e73468
Dario <dario_gow3@hotmail.com>
2019-03-12 21:19:28 -0600

    Newton-Raphson terminado
    al fin

--- Newton-Raphson/Newton-Raphson/src/NewtonRaphson.java
+++ Newton-Raphson/Newton-Raphson/src/NewtonRaphson.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class NewtonRaphson {
 
@@ -6,6 +7,26 @@ public class NewtonRaphson {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        
+        double pot = 0, aprox = 0;
+        
+         double [][] termino = new double [100][2];
+        
+        System.out.println("Ingresar la potencia mas alta de la ecuacion");
+        pot = in.nextInt();
+        
+        System.out.println("Ingresar la aproximación inicial");
+        aprox = in.nextDouble();
+        
+        for(int i=0; i<=pot; i++){
+            System.out.println("Ingresar valor de x^"+i);
+            termino[i][0] = in.nextDouble();
+            termino[i][1] = i;
+        }
+
+        Procedimiento proc = new Procedimiento(termino, pot);
+        double x = proc.calculo(aprox);
     }
     
 }
---
+++ Newton-Raphson/Newton-Raphson/src/Procedimiento.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dario
+ */
+public class Procedimiento {
+    public double[][] termino, der;
+    public double  pot;
+
+    public Procedimiento(double[][] termino,  double pot) {
+        this.termino = termino;
+        this.der = new double[100][2];
+        this.der = der;
+        this.pot = pot;
+         for(int i=1; i<=pot; i++){
+            der[i-1][0] = termino[i][0]*termino[i][1];
+            der[i-1][1] = termino[i][1]-1;
+        }
+    }
+    
+    public double calculo(double xi){
+       double x = xi;
+       double res = 0;
+       double res_der = 0;
+       
+       for(int i = 0; i<=pot; i++){
+           res = res + termino[i][0]*(Math.pow(x, termino[i][1]));
+           res_der = res_der + der[i][0]*(Math.pow(x, der[i][1]));
+       }
+       
+        System.out.println(" R = "+res+"    R_derivada = "+res_der+"    x = "+x);
+        
+        x = x - (res/res_der);
+        
+        if(Math.abs(res)<=.0000001){
+            return xi;
+        }
+        else{
+            return calculo(x);
+        }
+    }
+    
+}
